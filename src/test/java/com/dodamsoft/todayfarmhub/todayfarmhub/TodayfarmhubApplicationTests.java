9729236f5826d888aa6856efde55b6efd5f415f1
seonhakji <jshag1990@gmail.com>
2024-05-27 23:53:49 +0900

    [Init] 프로젝트 초기화

---
+++ src/test/java/com/dodamsoft/todayfarmhub/todayfarmhub/TodayfarmhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dodamsoft.todayfarmhub.todayfarmhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayfarmhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
