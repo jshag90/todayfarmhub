beb456f5c50e62fbd22bd935de225490e4f35da5
seonhakji <jshag1990@gmail.com>
2024-06-12 22:55:53 +0900

    [Feat] 카테고리에 해당하는 가격 데이터 존재 여부 검사, 데이터 조회 메서드 추가

--- src/main/java/com/dodamsoft/todayfarmhub/repository/PricesRepository.java
+++ src/main/java/com/dodamsoft/todayfarmhub/repository/PricesRepository.java
@@ -1,11 +1,29 @@
 package com.dodamsoft.todayfarmhub.repository;
 
 import com.dodamsoft.todayfarmhub.entity.Prices;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PricesRepository extends JpaRepository<Prices, Long> {
 
+    /**
+     * 금액 테이블에 해당 날짜, lclass, mclass, sclass 카테고리에 해당하는 데이터가 있는지 조회
+     *
+     * @param dates
+     * @param lClassCodeId
+     * @param mClassCodeId
+     * @param sClassCodeId
+     * @return
+     */
+    @Query("SELECT CASE WHEN COUNT(p) > 0 THEN true ELSE false END " +
+            "FROM Prices p " +
+            "WHERE p.dates = ?1 AND p.lClassCode.id = ?2 AND p.mClassCode.id = ?3 AND p.sClassCode.id = ?4")
+    boolean existsByDatesAndLClassCodeIdAndMClassCodeIdAndSClassCodeId(String dates, Long lClassCodeId, Long mClassCodeId, Long sClassCodeId);
+
+    Page<Prices> findByDatesAndLClassCodeIdAndMClassCodeIdAndSClassCodeId(String dates, Long lClassCodeId, Long mClassCodeId, Long sClassCodeId, Pageable pageable);
 
 }
