892b52d6b0b9c951279cf3af4435fdfe9db3c2fc
seonhakji <jshag1990@gmail.com>
2024-06-27 16:32:48 +0900

    [Feat] AuctionPricesSaveScheduler 초기화 제거

--- src/main/java/com/dodamsoft/todayfarmhub/controller/AuctionController.java
+++ src/main/java/com/dodamsoft/todayfarmhub/controller/AuctionController.java
@@ -35,8 +35,6 @@ public class AuctionController {
     @Qualifier("marketCategoryService")
     private final GetAuctionCategoryService marketCategoryService;
 
-    private final AuctionPricesSaveScheduler auctionPricesSaveScheduler;
-
     @GetMapping("/prices")
     public ResponseEntity getAuctionPrices(
               @RequestParam("date") String date
