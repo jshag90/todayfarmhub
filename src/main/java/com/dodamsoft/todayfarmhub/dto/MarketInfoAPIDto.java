9c4ed576ad29bf3e61c6ecd2f02d7c9e50183f33
seonhakji <jshag1990@gmail.com>
2025-11-06 17:55:47 +0900

    [Feat] dto 변경에 따른 코드 수정

--- src/main/java/com/dodamsoft/todayfarmhub/dto/MarketInfoAPIDto.java
+++ src/main/java/com/dodamsoft/todayfarmhub/dto/MarketInfoAPIDto.java
@@ -5,21 +5,65 @@ import lombok.*;
 import java.util.List;
 
 @Builder
-@Setter
 @Getter
+@Setter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class MarketInfoAPIDto {
 
-    List<ResultList> resultList;
+    private Response response;
 
     @Builder
     @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Response {
+        private Header header;
+        private Body body;
+    }
+
+    @Builder
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Header {
+        private String resultCode;
+        private String resultMsg;
+    }
+
+    @Builder
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Body {
+        private String dataType;
+        private int numOfRows;
+        private int pageNo;
+        private int totalCount;
+        private Items items;
+    }
+
+    @Builder
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Items {
+        private List<ResultList> item;
+    }
+
+    // 기존 유지 (이름만 marketCode → whsl_mrkt_cd, marketName → whsl_mrkt_nm)
+    @Builder
+    @Getter
+    @Setter
     @NoArgsConstructor
     @AllArgsConstructor
     public static class ResultList {
-        String marketCode;
-        String marketName;
+        private String whsl_mrkt_cd;
+        private String whsl_mrkt_nm;
     }
 }
--- src/main/java/com/dodamsoft/todayfarmhub/service/MarketCategoryService.java
+++ src/main/java/com/dodamsoft/todayfarmhub/service/MarketCategoryService.java
@@ -33,7 +33,7 @@ public class MarketCategoryService implements GetAuctionCategoryService {
     private final Gson gson;
 
     @Override
-    public <T> T getCategory(AuctionPriceVO auctionPriceVO) {
+    public MarketInfoAPIDto getCategory(AuctionPriceVO auctionPriceVO) {
 
         AuctionAPIVO.Market auctionAPIVO = AuctionAPIVO.Market.builder()
                 .lClassCode("")
@@ -51,40 +51,77 @@ public class MarketCategoryService implements GetAuctionCategoryService {
                 .sortGbn("")
                 .build();
 
-        log.info(auctionAPIVO.toString());
+        log.info("AuctionAPIVO: {}", auctionAPIVO);
 
-        if(marketCodeRepository.count() < 1) {
+        // DB에 데이터 없으면 API로 가져와서 저장
+        if (marketCodeRepository.count() < 1) {
             saveInfoByResponseDataUsingAPI(auctionAPIVO, null, null);
         }
 
-        List<MarketInfoAPIDto.ResultList> resultList = new ArrayList<>();
+        // "서울" 키워드로 정렬된 시장 목록 조회
         List<MarketCode> marketNameList = marketCodeRepository.findAllOrderedByNameWithKeywordFirst("서울");
-        for (MarketCode marketCode : marketNameList) {
-                resultList.add(MarketInfoAPIDto.ResultList.builder()
-                        .marketCode(marketCode.getMarketCode())
-                        .marketName(marketCode.getMarketName())
-                        .build());
-        }
 
-        return (T) MarketInfoAPIDto.builder().resultList(resultList).build();
+        // ResultList로 변환
+        List<MarketInfoAPIDto.ResultList> itemList = marketNameList.stream()
+                .map(marketCode -> MarketInfoAPIDto.ResultList.builder()
+                        .whsl_mrkt_cd(marketCode.getMarketCode())
+                        .whsl_mrkt_nm(marketCode.getMarketName())
+                        .build())
+                .toList();
+
+        // 전체 응답 구조 생성
+        MarketInfoAPIDto.Items items = MarketInfoAPIDto.Items.builder()
+                .item(itemList)
+                .build();
+
+        MarketInfoAPIDto.Body body = MarketInfoAPIDto.Body.builder()
+                .dataType("JSON")
+                .numOfRows(itemList.size())
+                .pageNo(1)
+                .totalCount(itemList.size())
+                .items(items)
+                .build();
+
+        MarketInfoAPIDto.Header header = MarketInfoAPIDto.Header.builder()
+                .resultCode("0")
+                .resultMsg("정상")
+                .build();
+
+        MarketInfoAPIDto.Response response = MarketInfoAPIDto.Response.builder()
+                .header(header)
+                .body(body)
+                .build();
+
+        return MarketInfoAPIDto.builder()
+                .response(response)
+                .build();
     }
 
     @Override
     public <T> void saveInfoByResponseDataUsingAPI(T t, LClassCode lClassCode, MClassCode mClassCode) {
         try {
 
-            String responseData = HttpCallUtil.getHttpPost(OriginAPIUrlEnum.GET_MARKET_INFO_URL.getUrl(), gson.toJson(t));
+
+            // 사용 예시 (완성된 URL 생성)
+            String getMarketInfoUrl = OriginAPIUrlEnum.GET_MARKET_INFO_URL.getUrl() + "?"
+                    + "serviceKey=7661d3c8bad3519c927fa736cc3214fed973dad9520645c34a1a1f1f20344d46"
+                    + "&pageNo=1"
+                    + "&numOfRows=33"
+                    + "&returnType=json"
+                    + "&selectable=whsl_mrkt_cd,whsl_mrkt_nm";
+
+            String responseData = HttpCallUtil.getHttpGet(OriginAPIUrlEnum.GET_MARKET_INFO_URL.getUrl());
             log.info(responseData);
 
             MarketInfoAPIDto marketInfoAPIDto = gson.fromJson(responseData, MarketInfoAPIDto.class);
-            for (MarketInfoAPIDto.ResultList resultList : marketInfoAPIDto.getResultList()) {
+            for (MarketInfoAPIDto.ResultList resultList : marketInfoAPIDto.getResponse().getBody().getItems().getItem()) {
 
-                if (marketCodeRepository.existsByMarketCode(resultList.getMarketCode()))
+                if (marketCodeRepository.existsByMarketCode(resultList.getWhsl_mrkt_cd()))
                     continue;
 
                 marketCodeRepository.save(MarketCode.builder()
-                        .marketName(resultList.getMarketName())
-                        .marketCode(resultList.getMarketCode())
+                        .marketName(resultList.getWhsl_mrkt_nm())
+                        .marketCode(resultList.getWhsl_mrkt_cd())
                         .build());
             }
 
